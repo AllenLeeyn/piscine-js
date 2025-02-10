8f77668b926589c6b0d6800d1b60f54cc45532fc
ylee <leeyn.shun@gmail.com>
2025-02-10 15:28:58 +0200

    dog-years.js: remove console.log and implement rounding.

--- dog-years.js
+++ dog-years.js
@@ -1,15 +1,16 @@
 const secondsInYear = 31557600;
 const planetYear = {
     'earth': 1,
-    'mecury': 0.2408467,
-    'venus': 0.61519736,
-    'mars': 1.8808158,
-    'jupiter': 11.862615,
-    'saturn': 29.447498,
-    'uranus': 84.016846,
-    'neptune': 164.79132
+    'mecury': 0.24,
+    'venus': 0.61,
+    'mars': 1.88,
+    'jupiter': 11.86,
+    'saturn': 29.44,
+    'uranus': 84.01,
+    'neptune': 164.79
 };
 
-const dogYears = (planetName, dogAgeSecs) => (dogAgeSecs/secondsInYear) * planetYear[planetName] * 7;
-
-console.log(dogYears('earth', 1000000000));
+const dogYears = (planetName, dogAgeSecs) => {
+    let result = (dogAgeSecs/secondsInYear) * planetYear[planetName] * 7;
+    return Math.round(result * 100) / 100;
+};
