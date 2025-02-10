d0410bb4e9b77983d96849534aec34240c3a3db2
ylee <leeyn.shun@gmail.com>
2025-02-10 18:26:05 +0200

    is.js: more test and check...

--- is.js
+++ is.js
@@ -16,7 +16,7 @@ is.falsy: value is falsy.
 */
 
 is.num = (n) => typeof n === 'number' && !Number.isNaN(n);
-is.nan = (n) => typeof n === 'number' && !Number.isNaN(n);
+is.nan = (n) => Number.isNaN(n);
 is.str = (n) => typeof n === 'string';
 is.bool = (n) => typeof n === 'boolean';
 is.undef = (n) => n === undefined;
