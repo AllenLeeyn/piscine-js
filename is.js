93fbcb53f77272132f73cba9129fe0226bdaa13f
ylee <leeyn.shun@gmail.com>
2025-02-10 18:38:00 +0200

    is.js: Run the test cases manually. Lets see if I am on the right track.

--- is.js
+++ is.js
@@ -14,8 +14,9 @@ is.fun: value is a function.
 is.truthy: value is truthy.
 is.falsy: value is falsy.
 */
+const is = {}
 
-is.num = (n) => typeof n === 'number' && !Number.isNaN(n);
+is.num = (n) => typeof n === 'number';
 is.nan = (n) => Number.isNaN(n);
 is.str = (n) => typeof n === 'string';
 is.bool = (n) => typeof n === 'boolean';
@@ -28,3 +29,22 @@ is.falsy = (n) =>{
  return n === null || is.undef(n) || n === false || is.nan(n) || n === 0 || n === -0 || n === 0n || n === "";
 }; 
 is.truthy = (n) => !is.falsy(n);
+
+/* 
+const t = (f) => tests.push(f)
+console.log(is.nan(0));
+console.log(is.nan(NaN));
+console.log(is.nan(true));
+console.log(is.nan(''));
+console.log(is.nan('💩'));
+console.log(is.nan(undefined));
+console.log(is.nan(t));
+console.log(is.nan([]));
+console.log(is.nan({}));
+console.log(is.nan([1, Array(1), [], 2]));
+console.log(is.nan({length: 10}));
+console.log(is.nan(Object.create(null)));
+console.log(is.nan(null));
+console.log(is.nan(console.log));
+console.log(is.nan(void 0)); 
+*/
