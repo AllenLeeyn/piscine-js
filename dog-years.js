6791fc26b2af2a37ad9ef5135bf28572353c5c4d
ylee <leeyn.shun@gmail.com>
2025-02-10 15:44:41 +0200

    dog-years.js: Change calculation

--- dog-years.js
+++ dog-years.js
@@ -1,16 +1,18 @@
 const secondsInYear = 31557600;
 const planetYear = {
     'earth': 1,
-    'mercury': 0.24,
-    'venus': 0.61,
-    'mars': 1.88,
-    'jupiter': 11.86,
-    'saturn': 29.44,
-    'uranus': 84.01,
-    'neptune': 164.79
+    'mercury': 0.2408467,
+    'venus': 0.61519726,
+    'mars': 1.8808156,
+    'jupiter': 11.862615,
+    'saturn': 29.447498,
+    'uranus': 84.016846,
+    'neptune': 164.79132
 };
 
 const dogYears = (planetName, dogAgeSecs) => {
-    let result = (dogAgeSecs/secondsInYear) * planetYear[planetName] * 7;
+    let result = (dogAgeSecs/secondsInYear) / planetYear[planetName] * 7;
     return Math.round(result * 100) / 100;
 };
+
+console.log(dogYears('mercury', 2134835688));
