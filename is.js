599c47227a91bf6a775b49a82164aadc07068645
ylee <leeyn.shun@gmail.com>
2025-02-10 18:02:29 +0200

    is.js

---
+++ is.js
@@ -0,0 +1,30 @@
+/*
+Add new function properties to the is object to check value types. 
+Each function should take one argument, and return a boolean.
+
+is.num: value is a number.
+is.nan: value is NaN.
+is.str: value is a string.
+is.bool: value is a boolean.
+is.undef: value is undefined.
+is.def: value is defined.
+is.arr: value is an array.
+is.obj: value is a simple object or null objects.
+is.fun: value is a function.
+is.truthy: value is truthy.
+is.falsy: value is falsy.
+*/
+
+is.num = (n) => typeof n === 'number';
+is.nan = (n) => typeof n === 'Nan';
+is.str = (n) => typeof n === 'string';
+is.bool = (n) => typeof n === 'boolean';
+is.undef = (n) => typeof n === 'undefined';
+is.def = (n) => typeof n !== 'undefined';
+is.arr = (n) => Array.isArray(n);
+is.obj = (n) => typeof n === 'object' || typeof n === null;
+is.fun = (n) => typeof n === 'function';
+is.falsy = (n) =>{
+ return n === null || is.undef(n) || n === false || is.nan(n) || n === 0 || n === -0 || n === 0n || n === "";
+}; 
+is.truthy = (n) => !is.falsy(n);
