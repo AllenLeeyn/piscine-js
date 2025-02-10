aed9c3a3d29affc49e88cb391455738dc0438eda
ylee <leeyn.shun@gmail.com>
2025-02-10 14:05:52 +0200

    mutability.js: fix typo

--- mutability.js
+++ mutability.js
@@ -1,6 +1,6 @@
 const clone1 = Object.assign({},person);
 const clone2 = Object.assign({},person);
 
-const samePerson = persona;
+const samePerson = person;
 person.age = person.age +1;
 person.country = 'FR';
