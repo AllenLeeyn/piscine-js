e7488b1b4dc9d06503768c8b21d0ff4b15eb8ad6
ylee <leeyn.shun@gmail.com>
2025-02-10 14:52:41 +0200

    dog-years.js

---
+++ dog-years.js
@@ -0,0 +1,13 @@
+const secondsInYear = 31557600;
+const planetYear = {
+    earth: 1,
+    mecury: 0.2408467,
+    venus: 0.61519736,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+};
+
+const dogYears = (planetName, dogAgeSecs) => (dogAgeSecs/secondsInYear) * planetYear[planetName];
