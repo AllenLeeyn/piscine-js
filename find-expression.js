59a663099a60aee118cea9551cd6ccee5bfe576f
ylee <leeyn.shun@gmail.com>
2025-02-12 15:29:10 +0200

    find-expression.js: change logic to consider if divisible by prime first (other than 5).

--- find-expression.js
+++ find-expression.js
@@ -1,14 +1,13 @@
-
 const findExpression = (num) => {
     let result = [];
     for (;num !== 1;){
-        if (num%2 === 0){
-            result.push(mul2);
-            num = num/2;
-        } else {
+        if (num%3 === 0 || num%7 === 0 || num%11 == 0 || num %2 !== 0){
             result.push(add4);
             num = num-4;
-        };
+        } else if (num%2 === 0){
+            result.push(mul2);
+            num = num/2;
+        }
         if (num < 1){
             return undefined;
         };
