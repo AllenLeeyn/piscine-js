c33e6ab8575486ce43ac64e3394b63258f161c8f
ylee <leeyn.shun@gmail.com>
2025-02-12 15:40:28 +0200

    find-expression.js: tester doesn't allow number from 5 to 9.

--- find-expression.js
+++ find-expression.js
@@ -1,7 +1,8 @@
+
 const findExpression = (num) => {
     let result = [];
     for (;num !== 1;){
-        if (num%3 === 0 || num%7 === 0 || num%11 == 0 || num %2 !== 0){
+        if (num%3 === 0 || num%(3+4) === 0 || num%11 == 0 || num %2 !== 0){
             result.push(add4);
             num = num-4;
         } else if (num%2 === 0){
