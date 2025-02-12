c758291d7287880e0528485d49bd037e5e93efb4
ylee <leeyn.shun@gmail.com>
2025-02-12 15:08:59 +0200

    find-expression.js: fix break condition. Was checking for 0 instead 1.

--- find-expression.js
+++ find-expression.js
@@ -1,17 +1,17 @@
+const add4 = '+4'
+const mul2 = '*2'
+
 const findExpression = (num) => {
-    result = '';
-    for (;num !== 0;){
+    let result = '';
+    for (;num !== 1;){
         if (num%2 === 0){
             result = result + mul2;
             num = num/2;
         } else {
             result = add4 + result;
             num = num-4;
-        }
-        if (num === 0){
-            break;
         };
-        if (num < 0){
+        if (num < 1){
             return undefined;
         };
     };
