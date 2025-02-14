dfc88a59a391066cab9e6e872521b9302385a7dc
ylee <leeyn.shun@gmail.com>
2025-02-14 09:41:03 +0200

    letter-space-number.js: update regex to not include last char.

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,5 +1,5 @@
 
-const ltrSpcNumRegExp = /(^|)[a-zA-Z]\s[0-9](\W|$)/g;
+const ltrSpcNumRegExp = /[a-zA-Z]\s[0-9](?=\s|[,.]|$)/g;
 
 const letterSpaceNumber = (str) => {
     return str.match(ltrSpcNumRegExp);
