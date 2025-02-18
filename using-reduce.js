f43f69b25fe14e20388b67faafe18c0efd4c7605
ylee <leeyn.shun@gmail.com>
2025-02-18 09:13:46 +0200

    using-reduce.js: Change how undefined initial value is handled.

--- using-reduce.js
+++ using-reduce.js
@@ -1,23 +1,22 @@
 const adder = (arr, initNum) => {
     if (!Array.isArray(arr)) return;
-    if (initNum !== undefined) return arr.reduce((sumVal, curVal) => sumVal + curVal, initNum);
-    return arr.reduce((sumVal, curVal) => sumVal + curVal);
+    if (initNum === undefined) initNum = 0;
+    return arr.reduce((sumVal, curVal) => sumVal + curVal, initNum);
 };
 
 const sumOrMul = (arr, initNum) => {
     if (!Array.isArray(arr)) return;
-    if (initNum !== undefined) return arr.reduce((sumVal, curVal) => {
-        if (curVal%2 == 0) return sumVal * curVal;
-        if (curVal%2 == 1 || curVal%2 == -1) return sumVal + curVal;
-    }, initNum);
+    if (initNum === undefined) initNum = 0;
     return arr.reduce((sumVal, curVal) => {
         if (curVal%2 == 0) return sumVal * curVal;
         if (curVal%2 == 1 || curVal%2 == -1) return sumVal + curVal;
-    });
+    }, initNum);
 };
 
 const funcExec = (arr, initNum) => {
     if (!Array.isArray(arr)) return;
-    if (initNum !== undefined) return arr.reduce((sumVal, curFunc) => curFunc(sumVal), initNum);
-    return arr.reduce((sumVal, curFunc) => curFunc(sumVal));
+    if (initNum === undefined) initNum = 0;
+    return arr.reduce((sumVal, curFunc) => curFunc(sumVal), initNum);
 };
+
+console.log(adder([]))//, 0))
