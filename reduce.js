cea9d307fcf510da68b079be517b7fa044746f24
ylee <leeyn.shun@gmail.com>
2025-02-18 10:31:28 +0200

    reduce.js: modify accumulator to be first arr value.

--- reduce.js
+++ reduce.js
@@ -1,15 +1,9 @@
-const forEach = (arr, fn) => {
-    for (let i = 0; i < arr.length; i++){
-        fn(arr[i], i, arr);
-    };
-};
-
 const reduce = (arr, fn, acc) => {
     if (acc !== undefined){
         arr = [acc, ...arr];
     };
-    let result = 0;
-    for (let i = 0; i < arr.length; i++){
+    let result = arr[0];
+    for (let i = 1; i < arr.length; i++){
         result = fn(result, arr[i]);
     };
     return result;
