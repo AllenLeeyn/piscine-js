cc043f84bd97da9a71f07d982fa6a832b9e95292
ylee <leeyn.shun@gmail.com>
2025-02-20 11:37:06 +0200

    fifty-shades-of-cold.js

---
+++ fifty-shades-of-cold.js
@@ -0,0 +1,24 @@
+import {colors} from './fifty-shades-of-cold.data.js';
+const head = document.head;
+
+export function generateClasses(){
+    const container = document.createElement('style');
+    colors.forEach((color) => container.innerHTML+=`.${color} {background: ${color};}\n`);
+    head.appendChild(container);
+};
+
+export function generateColdShades(){
+    colors.forEach((color) => {
+        if (/(aqua|blue|turquoise|green|vyan|navy|purple)/.test(color)){
+            const newDiv = document.createElement('div');
+            newDiv.classList.add(color);
+            newDiv.textContent = color
+            document.body.appendChild(newDiv);
+        };
+    });
+};
+
+export function choseShade(color){
+    const elements = Array.from(document.getElementsByTagName('div'))
+    elements.forEach((ele) => {ele.classList = color});
+};
