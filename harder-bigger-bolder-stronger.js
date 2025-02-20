221a07bd337a8a3b0f7838da5b46508cebca4d7e
ylee <leeyn.shun@gmail.com>
2025-02-20 10:03:00 +0200

    harder-bigger-bolder-stronger.js

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,15 @@
+export function generateLetters(){
+    const body = document.body;
+    const count = 120;
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const initFontSize = 11;
+    const fontWeight = [300, 400, 600];
+
+    for (let i = 0; i < count; i++){
+        const ele = document.createElement('div');
+        ele.textContent = alphabet[Math.floor(Math.random()*26)];
+        ele.style.fontSize = (initFontSize + i + 'px');
+        ele.style.fontWeight = fontWeight[i%3];
+        body.appendChild(ele);
+    }
+}
