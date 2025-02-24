064e10f10a14116f68238220926bf89d3927e19a
ylee <leeyn.shun@gmail.com>
2025-02-24 18:19:58 +0200

    replica.js: change to handle multiple objs

--- replica.js
+++ replica.js
@@ -1,15 +1,18 @@
-const replica = (tgt, obj) => {
-    if (obj instanceof RegExp) return new RegExp(obj.source, obj.flags);
+const replica = (tgt, ...objs) => {
 
-    if (typeof obj === 'object'){
-        for (const [key, val] of Object.entries(obj)){
-            if (typeof val === obj) {
-                tgt[key] = replica(tgt[key], val);
-            } else {
-                tgt[key] = val;
-            }
-        };
-        return tgt;
-    }
-    return obj;
+    objs.forEach(obj => {
+        if (obj instanceof RegExp) return new RegExp(obj.source, obj.flags);
+
+        if (typeof obj === 'object'){
+            for (const [key, val] of Object.entries(obj)){
+                if (typeof val === obj) {
+                    tgt[key] = replica(tgt[key], val);
+                } else {
+                    tgt[key] = val;
+                }
+            };
+        }
+        return obj;
+    });
+    return tgt;
 };
