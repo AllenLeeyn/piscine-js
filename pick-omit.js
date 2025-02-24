e6035ac60e026baac4e6e527f1403485aafbb6ab
ylee <leeyn.shun@gmail.com>
2025-02-24 11:30:16 +0200

    pick-omit.js: fix typeof string check

--- pick-omit.js
+++ pick-omit.js
@@ -1,17 +1,17 @@
 const pick = (obj, strArr) => {
-    if (typeof strArr === 'String') strArr = [strArr];
+    if (typeof strArr === 'string') strArr = [strArr];
     const objArr = [];
     for (const [key, value] of  Object.entries(obj)){
-        for (const str of strArr) (str === String(key)) ? objArr.push([key, value]):null;
+        (strArr.includes(key)) ? objArr.push([key, value]):null;
     };
     return Object.fromEntries(objArr);
 };
 
 const omit = (obj, strArr) => {
-    if (typeof strArr === 'String') strArr = [strArr];
+    if (typeof strArr === 'string') strArr = [strArr];
     const objArr = [];
     for (const [key, value] of  Object.entries(obj)){
-        for (const str of strArr) (str !== String(key)) ? objArr.push([key, value]):null;
+        (!strArr.includes(key)) ? objArr.push([key, value]):null;
     };
     return Object.fromEntries(objArr);
 };
