26ec453569b0232e19da472b34cdeb5820d98164
ylee <leeyn.shun@gmail.com>
2025-02-24 18:29:57 +0200

    replica.js: fix flawed logic

--- replica.js
+++ replica.js
@@ -1,11 +1,11 @@
 const replica = (tgt, ...objs) => {
 
     objs.forEach(obj => {
-        if (obj instanceof RegExp) return new RegExp(obj.source, obj.flags);
+        if (obj instanceof RegExp) return tgt.push(new RegExp(obj.source, obj.flags));
 
         if (typeof obj === 'object'){
             for (const [key, val] of Object.entries(obj)){
-                if (typeof val === obj) {
+                if (typeof val === 'object') {
                     tgt[key] = replica(tgt[key], val);
                 } else {
                     tgt[key] = val;
