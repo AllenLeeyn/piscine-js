73179f2a1755329c1952f860dc1c4b03a7615fa4
ylee <leeyn.shun@gmail.com>
2025-02-24 18:15:00 +0200

    replica.js

---
+++ replica.js
@@ -0,0 +1,15 @@
+const replica = (tgt, obj) => {
+    if (obj instanceof RegExp) return new RegExp(obj.source, obj.flags);
+
+    if (typeof obj === 'object'){
+        for (const [key, val] of Object.entries(obj)){
+            if (typeof val === obj) {
+                tgt[key] = replica(tgt[key], val);
+            } else {
+                tgt[key] = val;
+            }
+        };
+        return tgt;
+    }
+    return obj;
+};
