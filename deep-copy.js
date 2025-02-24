bd9b19b56cac4c8e17a880107bc571e6c06dab44
ylee <leeyn.shun@gmail.com>
2025-02-24 17:36:31 +0200

    deep-copy.js: correct flawed logic.

--- deep-copy.js
+++ deep-copy.js
@@ -4,7 +4,8 @@ const deepCopy = (obj) => {
         for (const val of obj){
             objArr.push(deepCopy(val));
         };
-    } else {
+        return objArr;
+    } else if (typeof obj === 'object' && obj !== null){
         for (const [key, val] of Object.entries(obj)){
             objArr.push([key,deepCopy(val)]);
         };
