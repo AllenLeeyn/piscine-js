17fd1406b2fff8931bfffaadb4cd8bb03f1fed1b
ylee <leeyn.shun@gmail.com>
2025-02-24 17:23:20 +0200

    deep-copy.js: change logic to handle arrays

--- deep-copy.js
+++ deep-copy.js
@@ -1,9 +1,16 @@
 const deepCopy = (obj) => {
     const objArr = [];
-    for (const [key, val] of Object.entries(obj)){
-        if (typeof val === 'object') {
-            objArr.push([key, deepCopy(val)])
-        } else objArr.push([key, val]);
-    }
-    return Object.fromEntries(objArr);
+    if (Array.isArray(obj)){
+        for (const val of obj){
+            objArr.push(deepCopy(val));
+        };
+    } else {
+        for (const [key, val] of Object.entries(obj)){
+            if (typeof val === 'object') {
+                objArr.push([key, deepCopy(val)])
+            } else objArr.push([key, val]);
+        };
+        return Object.fromEntries(objArr);
+    };
+    return obj;
 };
