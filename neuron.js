dfeed28c1d7eb84a3ac5abe8faab3ca674ea8ff8
ylee <leeyn.shun@gmail.com>
2025-02-24 20:28:14 +0200

    nueron.js: changed how a key is populated.

--- neuron.js
+++ neuron.js
@@ -18,45 +18,10 @@ const neuron = (arr) => {
         if (obj[promptType][qKey]) {
             curResponse = obj[promptType][qKey].responses;
         };
-        if (promptType === 'questions'){
-            obj[promptType][qKey] = {
-                question: qVal,
-                responses: [...curResponse,rVal]
-            };
-        } else {
-            obj[promptType][qKey] = {
-                order: qVal,
-                responses: [...curResponse,rVal]
-            };
-        }
-        
+        obj[promptType][qKey] = {
+            [promptType.slice(0, -1)]: qVal,
+            responses: [...curResponse,rVal]
+        };
     });
     return obj;
 };
-
-/* console.log(neuron([
-    'Questions: what is ounces? - Response: Ounce, unit of weight in the avoirdupois system',
-    'Questions: what is ounces? - Response: equal to 1/16 pound (437 1/2 grains)',
-    'Questions: what is Mud dauber - Response: Mud dauber is a name commonly applied to a number of wasps',
-    'Orders: shutdown! - Response: Yes Sr!',
-    'Orders: Quote something! - Response: Pursue what catches your heart, not what catches your eyes.'
-  ]))
-   */
-/*   // output
-  {
-    questions: {
-      what_is_ounces: { question: 'what is ounces?', responses: [
-          'Ounce, unit of weight in the avoirdupois system',
-          'equal to 1/16 pound (437 1/2 grains)'
-      ] },
-      what_is_mud_dauber: { question: 'what is Mud dauber', responses: [
-          'Mud dauber is a name commonly applied to a number of wasps'
-      ] }
-    },
-    orders: {
-      shutdown: { order: 'shutdown!', responses: ['Yes Sr!'] },
-      quote_something: { order: 'Quote something!', responses: [
-          'Pursue what catches your heart, not what catches your eyes.'
-      ] }
-    }
-  } */
