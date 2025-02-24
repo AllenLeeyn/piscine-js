6624dcb0646a086732cf472ab2575d11352bae55
ylee <leeyn.shun@gmail.com>
2025-02-24 21:10:04 +0200

    flagger.js: fix error in handling help.

--- flagger.js
+++ flagger.js
@@ -1,13 +1,17 @@
 const flags = (data) => {
     const obj = { alias: {h: 'help'}, description: []}
-    let helpVals = obj.help;
+    const helpVals = (data.help) ? data.help: [];
+    const helpAll = (data.help === undefined);
+    
+    console.log(helpVals)
     for (const [key, val] of Object.entries(data)){
         if (key === 'help') continue;
 
         obj.alias[key.slice(0,1)] = key;
-        if (helpVals === undefined || helpVals.includes(key)){
-            obj.description.push(`-${key.slice(0,1)}, --${key}: ${val}`) 
-        }
+        if (helpAll) helpVals.push(key);
+    }
+    for (const key of helpVals){
+        obj.description.push(`-${key.slice(0,1)}, --${key}: ${data[key]}`) 
     }
     obj.description = obj.description.join('\n');
     return obj;
