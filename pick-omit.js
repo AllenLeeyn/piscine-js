49ac2ce12b1ab29f49ac472745b78181300456e9
ylee <leeyn.shun@gmail.com>
2025-02-24 11:27:16 +0200

    pick-omit.js: cast key into string

--- pick-omit.js
+++ pick-omit.js
@@ -2,7 +2,7 @@ const pick = (obj, strArr) => {
     if (typeof strArr === 'String') strArr = [strArr];
     const objArr = [];
     for (const [key, value] of  Object.entries(obj)){
-        for (const str of strArr) (str === key) ? objArr.push([key, value]):null;
+        for (const str of strArr) (str === String(key)) ? objArr.push([key, value]):null;
     };
     return Object.fromEntries(objArr);
 };
@@ -11,7 +11,7 @@ const omit = (obj, strArr) => {
     if (typeof strArr === 'String') strArr = [strArr];
     const objArr = [];
     for (const [key, value] of  Object.entries(obj)){
-        for (const str of strArr) (str !== key) ? objArr.push([key, value]):null;
+        for (const str of strArr) (str !== String(key)) ? objArr.push([key, value]):null;
     };
     return Object.fromEntries(objArr);
 };
