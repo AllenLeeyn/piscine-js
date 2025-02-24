190e77d2bcd03ff241bdfcb2269286b99374139e
ylee <leeyn.shun@gmail.com>
2025-02-24 19:35:20 +0200

    pronoun.js: change incorrect regexp.

--- pronoun.js
+++ pronoun.js
@@ -2,7 +2,9 @@ const pronouns = ['i', 'you', 'he', 'she', 'it', 'they', 'we'];
 
 const pronoun = (str) => {
     const obj = {};
-    const words = str.split(/[\s\p{P}]+/);
+    const words = str.split(/[\s\,\.]+/);
+
+    console.log(words);
 
     for (let i = 0; i < words.length; i++) {
         let word = words[i].toLowerCase();
@@ -17,3 +19,5 @@ const pronoun = (str) => {
     }
     return obj;
 };
+
+console.log(pronoun('I buy,\ni to,\nYOU buy,\nit have,\nIt buys,\nit is,\nyou go'));
