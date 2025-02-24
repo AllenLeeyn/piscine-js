0c4bbda16f5f7cf5915d7b4bab9bb8e51f3ec3da
ylee <leeyn.shun@gmail.com>
2025-02-24 19:38:20 +0200

    pronoun.js: handle case when pronoun if the last word.

--- pronoun.js
+++ pronoun.js
@@ -4,8 +4,6 @@ const pronoun = (str) => {
     const obj = {};
     const words = str.split(/[\s\,\.]+/);
 
-    console.log(words);
-
     for (let i = 0; i < words.length; i++) {
         let word = words[i].toLowerCase();
         if (pronouns.includes(word)){
@@ -14,10 +12,8 @@ const pronoun = (str) => {
             } else {
                 obj[word].count++;
             }
-            if (!pronouns.includes(words[i+1])) obj[word].word.push(words[i+1]);
+            if (!pronouns.includes(words[i+1]) && i+1 < words.length) obj[word].word.push(words[i+1]);
         }
     }
     return obj;
 };
-
-console.log(pronoun('I buy,\ni to,\nYOU buy,\nit have,\nIt buys,\nit is,\nyou go'));
