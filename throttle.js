dfa931230125a351bd23e136c171233189cc21e3
ylee <leeyn.shun@gmail.com>
2025-02-26 16:53:17 +0200

    throttle.js: default empty obj for oprions

--- throttle.js
+++ throttle.js
@@ -1,10 +1,8 @@
-const opThrottle = (func, wait = 0, options) => {
+const opThrottle = (func, wait = 0, options = {}) => {
     if (typeof func != 'function') {
       throw new TypeError(FUNC_ERROR_TEXT);
     }
     let lastCallTime = 0;
-    let lastArgs;
-    let lastThis;
     let timeout;
     let leading = (options.leading === undefined) ? false : options.leading;
     let trailing = (options.trailing === undefined) ? false : options.trailing;
