b311bbd6dd2b3cfe8d7b69b198c281645d3ca56a
ylee <leeyn.shun@gmail.com>
2025-02-28 11:07:05 +0200

    is-winner.js: make function async so I can use await keyword. Correct typos.

--- is-winner.js
+++ is-winner.js
@@ -1,17 +1,17 @@
-function isWinner(countryName){
-    const winner = db.getWinner(countryName).catch(()=>{
+async function isWinner(countryName){
+    const winner = await db.getWinner(countryName).catch(()=>{
         return `${countryName} never was a winner`;
     });
     if (winner === undefined) return `${countryName} never was a winner`;
 
-    if (winner.continent !== 'Europe') return `${countryName} is not what we are looking for because of the continent ${winner.continent}`;
+    if (winner.continent !== 'Europe') return `${countryName} is not what we are looking for because of the continent`;
 
-    const result = db.getResults(winner.id)
-    if (result.lenght < 3) return `${countryName} is not what we are looking for because of the number of times it was champion`;
+    const result = await db.getResults(winner.id);
+    if (result.length < 3) return `${countryName} is not what we are looking for because of the number of times it was champion`;
 
     let resultYear = [];
     let resultScore = [];
-    result.array.forEach(e => {
+    result.forEach(e => {
         resultYear.push(e.year);
         resultScore.push(e.score)
     });
