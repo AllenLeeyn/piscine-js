4650255024f43b7f77dce27b7f039afab48aadab
ylee <leeyn.shun@gmail.com>
2025-02-28 16:34:02 +0200

    race.js

--- race.js
+++ race.js
@@ -10,7 +10,7 @@ function race(promises){
 function some(promises, count){
     if (promises.length === 0 || count === 0) return [];
 
-    const results = new Array(promises.length);
+    const results = new Array(promises.length).fill(null);
     let resolvedCount = 0;
     return new Promise((resolve, reject) => {
         promises.forEach((promise, index) => {
@@ -20,7 +20,7 @@ function some(promises, count){
                 results[index] = result;
                 resolvedCount++;
 
-                if (resolvedCount === count) return resolve(results.filter(val => val !== undefined));
+                if (resolvedCount === count) return resolve(results.filter(val => val !== null));
             }).catch(reject)
         });
     });
