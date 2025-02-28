ac910d2e09633516c689cf2a63a255fad3093e23
ylee <leeyn.shun@gmail.com>
2025-02-28 11:34:22 +0200

    gougle-search.js: make function async

--- gougle-search.js
+++ gougle-search.js
@@ -1,4 +1,4 @@
-function queryServers(serverName, q){
+async function queryServers(serverName, q){
     const promise1 = new Promise(()=>`/${serverName}?q=${q}`);
     const promise2 = new Promise(()=>`/${serverName}_backup?q=${q}`);
     return Promise.race([promise1, promise2]);
