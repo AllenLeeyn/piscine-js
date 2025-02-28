ea36090e75b40ec8c3c952fe0d468840167be394
ylee <leeyn.shun@gmail.com>
2025-02-28 09:33:28 +0200

    get-json.js: return obj as requested

--- get-json.js
+++ get-json.js
@@ -2,10 +2,17 @@ function getJSON(path, params){
     const pathQueries = new URLSearchParams(params);
     const pathWithQueries = `${path}?${pathQueries.toString()}`;
 
-   return  fetch(pathWithQueries)
-        .then((response) => {
-            if (!response.ok) throw Error(response.statusText);
-            return response.json();
-        })
-        .catch((error) => {throw error;});
-};
+    let  errorMsg;
+    return fetch(pathWithQueries)
+            .then((response) => {
+                if (!response.ok) {
+                    errorMsg = response.statusText;
+                    throw Error(response.statusText)
+                };
+                return response.json();
+            })
+            .catch((error) => {throw error})
+            .then((json) => {
+                return{data: json, error: errorMsg}
+            });
+    };
