ffcbf6d26623d54177e10afa6a020073fa2d0542
ylee <leeyn.shun@gmail.com>
2025-02-28 10:48:13 +0200

    is-winner.js: not sure which keyword is causing error.

--- is-winner.js
+++ is-winner.js
@@ -1,20 +1,19 @@
 function isWinner(countryName){
-    try {
-        const winner = await db.getWinner(countryName);
-
-        if (winner.continent !== 'Europe') return `${countryName} is not what we are looking for because of the continent`;
-        
-        const result = await db.getResults(winner.id)
-        if (result.lenght < 3) return `${countryName} is not what we are looking for because of the number of times it was champion`;
-
-        let resultYear = [];
-        let resultScore = [];
-        result.array.forEach(e => {
-            resultYear.push(e.year);
-            resultScore.push(e.score)
-        });
-        return `${countryName} won the FIFA World Cup in ${resultYear.join(', ')} winning by ${resultScore.join(', ')}`;
-    } catch (error){
+    const winner = db.getWinner(countryName).catch(()=>{
         return `${countryName} never was a winner`;
-    }
+    });
+    if (winner === undefined) return `${countryName} never was a winner`;
+
+    if (winner.continent !== 'Europe') return `${countryName} is not what we are looking for because of the continent`;
+
+    const result = db.getResults(winner.id)
+    if (result.lenght < 3) return `${countryName} is not what we are looking for because of the number of times it was champion`;
+
+    let resultYear = [];
+    let resultScore = [];
+    result.array.forEach(e => {
+        resultYear.push(e.year);
+        resultScore.push(e.score)
+    });
+    return `${countryName} won the FIFA World Cup in ${resultYear.join(', ')} winning by ${resultScore.join(', ')}`;
 };
