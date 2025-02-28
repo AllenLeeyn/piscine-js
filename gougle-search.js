413ba370a7524de224ae0d3a316cafd690bb535e
ylee <leeyn.shun@gmail.com>
2025-02-28 11:38:50 +0200

    gougle-search.js: hmmm

--- gougle-search.js
+++ gougle-search.js
@@ -3,3 +3,11 @@ async function queryServers(serverName, q){
     const promise2 = new Promise(()=>`/${serverName}_backup?q=${q}`);
     return Promise.race([promise1, promise2]);
 };
+
+function gougleSearch(q){
+    const webUrl = queryServers('web', q);
+    const imageUrl = queryServers('image', q);
+    const videoUrl = queryServers('video', q);
+
+    return {web: getJSON(webUrl), image: getJSON(imageUrl), video: getJSON(videoUrl)};
+};
