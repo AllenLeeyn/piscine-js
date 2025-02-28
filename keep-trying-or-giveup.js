12efe1d126407554d6a0bcdb4354d8a36b0308a9
ylee <leeyn.shun@gmail.com>
2025-02-28 08:54:14 +0200

    keep-trying-or.giveup.js

---
+++ keep-trying-or-giveup.js
@@ -0,0 +1,26 @@
+function retry(count, callback){
+    let i = 0;
+
+    return async function again(...args){
+        return await callback(...args).catch(err => {
+            if (i >= count) throw err;
+            i++;
+            return again(...args);
+        });
+    }
+};
+
+function timeout(delay, callback){
+    const promise = new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve(new Error('timeout'))
+        }, delay);
+    });
+
+    return async function(...args){
+        return Promise.race([callback(...args), promise]).then(result=>{
+            if (result instanceof error) throw value;
+            return value;
+        });
+    };
+};
