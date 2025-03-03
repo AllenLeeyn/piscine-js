170cb8f81b2b83ede6bcaf7a3088ad3017c37b3a
ylee <leeyn.shun@gmail.com>
2025-03-03 19:01:41 +0200

    gatecrashers.mjs

--- gatecrashers.mjs
+++ gatecrashers.mjs
@@ -34,9 +34,6 @@ const postMethod = async (req, res) => {
 
     req.on('end', async () => {
         try {
-            await fs.mkdir('./guests', { recursive: true });
-            body = JSON.stringify(JSON.parse(body));
-
             const data = new Uint8Array(Buffer.from(body));
             const promise = fs.writeFile(`./guests${req.url}.json`, data);
             await promise;
