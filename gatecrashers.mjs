64ff262be44fcc2841956e89e821b4f56fb3bfc4
ylee <leeyn.shun@gmail.com>
2025-03-03 20:04:49 +0200

    gatecrashers.mjs

--- gatecrashers.mjs
+++ gatecrashers.mjs
@@ -22,43 +22,41 @@ const isBestFriends = (req) => {
     return true;
 }
 
-const postMethod = async (req, res) => {
-    if (!isBestFriends(req)){
-        res.writeHead(401, { 'Content-Type': 'application/json' });
-        res.end('Authorization Required');
-        return;
-    };
-
-    let resCode = 200;
-    let body = '';
-
-    req.on('data', chunk => {
-        body += chunk;
-    });
-
-    req.on('end', async () => {
-        try {
-            body = JSON.stringify(JSON.parse(body));
-            const filePath = path.join('guests', `${req.url}.json`);
-            const data = new Uint8Array(Buffer.from(body));
-            
-            fs.writeFile(filePath, data);
-
-            res.writeHead(resCode, { 'Content-Type': 'application/json'});
-            res.end(body);
-        } catch (error) {
-            resCode = 500;
-            body = JSON.stringify({ error: 'server failed'});
-
-            res.writeHead(resCode, { 'Content-Type': 'application/json'});
-            res.end(body);
-        }
-    });
-};
-
 server.on('request', async (req, res) =>{
     if (req.method === 'POST') {
-        await postMethod(req, res);
+        if (!isBestFriends(req)){
+            res.writeHead(401, { 'Content-Type': 'application/json' });
+            res.end('Authorization Required');
+            return;
+        };
+    
+        let resCode = 200;
+        let body = '';
+    
+        req.on('data', chunk => {
+            body += chunk;
+        });
+    
+        
+        req.on('end', async () => {
+            try {
+                body = JSON.stringify(JSON.parse(body));
+                if (!body ) body = req.headers['body'];
+                const filePath = path.join('guests', `${req.url}.json`);
+                const data = new Uint8Array(Buffer.from(body));
+                
+                await fs.writeFile(filePath, data);
+    
+                res.writeHead(resCode, { 'Content-Type': 'application/json'});
+                res.end(body);
+            } catch (error) {
+                resCode = 500;
+                body = JSON.stringify({ error: 'server failed'});
+    
+                res.writeHead(resCode, { 'Content-Type': 'application/json'});
+                res.end(body);
+            }
+        });
     };
 });
 
