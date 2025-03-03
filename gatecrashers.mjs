2280af66d219c4f31cc19ebb5e97d7804edef766
ylee <leeyn.shun@gmail.com>
2025-03-03 18:28:47 +0200

    gatecrashers.mjs: parse and stringify JSON to try and fix error.

--- gatecrashers.mjs
+++ gatecrashers.mjs
@@ -32,9 +32,10 @@ const postMethod = async (req, res) => {
     req.on('data', chunk => {
         body += chunk;
     });
-    
+
     req.on('end', async () => {
         try {
+            body = JSON.stringify(JSON.parse(body));
             const data = new Uint8Array(Buffer.from(body));
             const promise = fs.writeFile(`guests${req.url}.json`, data);
             await promise;
