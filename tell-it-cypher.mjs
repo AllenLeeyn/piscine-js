792d39bcd3e5ec8445a94a41abfd34c208d81241
ylee <leeyn.shun@gmail.com>
2025-03-03 13:20:20 +0200

    tell-it-cypher.mjs: handle the whole input as one

--- tell-it-cypher.mjs
+++ tell-it-cypher.mjs
@@ -2,31 +2,24 @@ import fs from 'fs/promises';
 
 const file = process.argv[2];
 const keyword = process.argv[3];
+let writefile = process.argv[4];
 
-const content = await fs.readFile(file, 'utf8');
-const lines = content.split("\n");
+let result;
 
-let writeFile;
-const results = [];
-for (const line of lines){
-    let result ;
-    if (keyword === 'encode') {
-        writeFile = 'cypher.txt';
-        result = Buffer.from(line, 'utf-8')
-        result = result.toString('base64');
-        results.push(result)
-    }
-    if (keyword === 'decode') {
-        writeFile = 'clear.txt';
-        result = Buffer.from(line, 'base64')
-        result = result.toString('utf-8');
-        results.push(result)
-    }
+if (keyword === 'encode') {
+    writefile = (!writefile) ? 'cypher.txt' : writefile;
+    const content = await fs.readFile(file);
+    result = content.toString('base64');
+}
+if (keyword === 'decode') {
+    writefile = (!writefile) ? 'clear.txt' : writefile;
+    const content = await fs.readFile(file, 'utf-8');
+    result = Buffer.from(content, 'base64');
+}
 
-};
 try {
-    const data = new Uint8Array(Buffer.from(results.join('\n')));
-    const promise = fs.writeFile(writeFile, data);
+    const data = new Uint8Array(Buffer.from(result));
+    const promise = fs.writeFile(writefile, data);
   
     await promise;
 } catch (err) {
