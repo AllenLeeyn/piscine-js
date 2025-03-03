3a906b3ced3e578b54b75c87aa116a42f1d20388
ylee <leeyn.shun@gmail.com>
2025-03-03 18:13:07 +0200

    gatecrashers.mjs

--- gatecrashers.mjs
+++ gatecrashers.mjs
@@ -3,11 +3,31 @@ import fs from 'fs/promises';
 
 const server = http.createServer();
 
+const bestFriends = ['Caleb_Squires', 'Tyrique_Dalton', 'Rahima_Young'];
+const pw = 'abracadabra';
+
+const isBestFriends = (req) => {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) return false;
+
+    const base64Credentials = authHeader.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('utf8');
+    const [username, password] = credentials.split(':');
+    if (pw !== password || !bestFriends.includes(username)) return false;
+
+    return true;
+}
+
 const postMethod = async (req, res) => {
+
+    if (!isBestFriends(req)){
+        res.writeHead(401, { 'Content-Type': 'application/json' });
+        res.end('Authorization Required');
+        return;
+    };
+
     let resCode = 201;
     let body = '';
-    
-    const fileName = req.url.slice(1);
 
     req.on('data', chunk => {
         body += chunk;
@@ -16,10 +36,9 @@ const postMethod = async (req, res) => {
     req.on('end', async () => {
         try {
             const data = new Uint8Array(Buffer.from(body));
-            const promise = fs.writeFile(`guests/${fileName}.json`, data);
+            const promise = fs.writeFile(`guests${req.url}.json`, data);
             await promise;
         } catch (error) {
-            console.log(error)
             resCode = 500;
             body = JSON.stringify({ error: 'server failed'});
         }
